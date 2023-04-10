d9166d911a5deeae098435eec5153afabc95c8f9
katiak2503 <katia_korndorfer@estudantes.sesisenai.org.br>
2023-04-10 08:56:44 -0300

    [M1S05-fix]

--- [M1S05]/script.js
+++ [M1S05]/script.js
@@ -16,50 +16,50 @@ console.log(mensagemOla("Kátia"));
 
 //exercício 3
 
-const arrayNum = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const arrayNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 let arrayInvertido = [];
 
 const inverteArray = () => {
-  for (let i = arrayNum.length - 1; i >= 0; i--) {
-    arrayInvertido.push(arrayNum[i]);
+  for (let i = arrayNumeros.length - 1; i >= 0; i--) {
+    arrayInvertido.push(arrayNumeros[i]);
   }
 }
 
 inverteArray();
 
-console.log("Array Original:", arrayNum);
+console.log("Array Original:", arrayNumeros);
 console.log("Array Invertido:", arrayInvertido);
 
 
 //exercício 4
 
-const arrayNum= [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const arrayNumeros= [1, 2, 3, 4, 5, 6, 7, 8, 9];
 let arrayImpares;
 
 const testarImpar = (valor) => {
   return valor % 2 !== 0;
 }
 
-arrayImpares = arrayNum.filter(testarImpar);
+arrayImpares = arrayNumeros.filter(testarImpar);
 
-console.log("Array Inicial:", arrayNum);
+console.log("Array Inicial:", arrayNumeros);
 console.log("Array de Ímpares:", arrayImpares);
 
-const arrayTodosImpares = arrayNum.every(testarImpar);
+const arrayTodosImpares = arrayNumeros.every(testarImpar);
 
 if (arrayTodosImpares) {
-  console.log("O vetor 'arrayImpares' possui todos os números ímpares da lista 'arrayNum'.");
+  console.log("O vetor 'arrayImpares' possui todos os números ímpares da lista 'arrayNumeros'.");
 } else {
-  console.log("O vetor 'arrayImpares' NÃO possui todos os números da lista 'arrayNum'.");
+  console.log("O vetor 'arrayImpares' NÃO possui todos os números da lista 'arrayNumeros'.");
 }
 
 //Exercício 5
 
-const arrayNum = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const arrayNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 const calculaQuadrado = (numero) => numero ** 2;
 
-const arrayQuadrados = arrayNum.map(calculaQuadrado);
+const arrayQuadrados = arrayNumeros.map(calculaQuadrado);
 
 console.log(arrayQuadrados);
 
